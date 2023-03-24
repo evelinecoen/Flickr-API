7e9a0798d58fd38153336b492ada23e6da8acc4c
“Eveline <eveline-c@hotmail.com>
2023-03-24 10:38:21 +0000

    final touches

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,25 +1,3 @@
-/* import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Favorites = ({ favorites }) => {
-  return (
-    <div>
-      <h2>Favorites</h2>
-    
-      <div>
-        {favorites.map((favorite, index) => (
-          <div key={index}>
-            <img className="favorite-image" src={favorite.srcPath} alt="favorite" />
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default Favorites;
-
- */
 import React from 'react';
 import { Link } from 'react-router-dom';
 
